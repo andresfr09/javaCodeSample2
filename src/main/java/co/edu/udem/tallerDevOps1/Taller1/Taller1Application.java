444888ca5243b59f814595a2662621a7b147b6b8
Andres Rodriguez <andres@gmail.com>
2021-10-14 08:50:28 -0500

    Commit 1 con Thymeleaf

---
+++ src/main/java/co/edu/udem/tallerDevOps1/Taller1/Taller1Application.java
@@ -0,0 +1,13 @@
+package co.edu.udem.tallerDevOps1.Taller1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taller1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Taller1Application.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/udem/tallerDevOps1/Taller1/Taller1ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.udem.tallerDevOps1.Taller1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taller1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
